0b3d391c6b1dd43aefd412dd1b0a03d15bf2b93f
Aditya chandel <adityachandel371@gmail.com>
2025-03-14 22:04:27 +0530

    Added new and reset button

--- src/App.js
+++ src/App.js
@@ -51,36 +51,58 @@ function Board() {
     setXIsNext(!xIsNext);
   }
 
+  // Reset game function
+  function resetGame() {
+    setSquares(Array(9).fill(null));
+    setXIsNext(true);
+  }
+
   // winner
   const winner = calculateWinner(squares);
+  // board is full or not
+  const isBoardFull = !squares.includes(null);
 
   let status;
   if (winner) {
-    status = "Winner: " + winner;
-  } else if (!squares.includes(null)) {
-    status = "Tie Game!";
+    status = "Congratulaitons!! The winner is " + winner;
+  } else if (isBoardFull) {
+    status = "Oops!! We have a tie game!";
   } else {
-    status = "Next player: " + (xIsNext ? "X" : "O");
+    status = "Next player : " + (xIsNext ? "X" : "O");
   }
 
   return (
     <>
 
-      <div className='game'>
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+      {!winner && !isBoardFull && (
+        <div className='game'>
+          <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+          <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+          <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
 
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+          <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+          <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+          <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
 
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
+          <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+          <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+          <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+        </div>
+      )}
 
       <div className="status">{status}</div>
+
+      {!winner && !isBoardFull && (
+        <div className="reset-btn">
+          <button onClick={resetGame}>Reset game</button>
+        </div>
+      )}
+
+      {(winner || isBoardFull) && (
+        <div className="new-game-btn">
+          <button onClick={resetGame}>New game</button>
+        </div>
+      )}
 
     </>
   );
